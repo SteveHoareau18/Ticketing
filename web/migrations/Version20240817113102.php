c7e933d6a122a7ed53266e11a7329e6300d084af
stevehoareau18 <steve.hoareau1@gmail.com>
2024-08-19 12:19:43 +0000

    [feature] add sql functions

--- web/migrations/Version20240817113102.php
+++ web/migrations/Version20240817113102.php
@@ -19,8 +19,32 @@ final class Version20240817113102 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('
+        DELIMITER //
+        CREATE PROCEDURE count_tickets_service(IN service_id INT)
+        BEGIN
+            SELECT 
+                (SELECT COUNT(*) FROM ticket WHERE service_id = service_id) AS total,
+                (SELECT COUNT(*) FROM ticket WHERE service_id = service_id AND result_date IS NULL) AS in_progress,
+                (SELECT COUNT(*) FROM ticket WHERE service_id = service_id AND result_date IS NOT NULL) AS closed;
+        END //
+        DELIMITER ;
+    ');
 
+        $this->addSql('
+        DELIMITER //
+        CREATE PROCEDURE count_tickets_user(IN user_id INT)
+        BEGIN
+            SELECT 
+                (SELECT COUNT(*) FROM treatment WHERE user_id = user_id) AS n_open,
+                (SELECT COUNT(*) FROM ticket WHERE creator_id = user_id) AS n_create,
+                (SELECT COUNT(*) 
+                 FROM treatment t
+                 JOIN ticket ti ON t.ticket_id = ti.id
+                 WHERE t.user_id = user_id AND t.end_date = ti.result_date) AS n_close;
+        END //
+        DELIMITER ;
+    ');
     }
 
     public function down(Schema $schema): void
