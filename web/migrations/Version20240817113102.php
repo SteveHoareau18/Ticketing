5c82b9a32617aae1fe2c3b34fdec2c3f6ce51aef
Steve <steve.hoareau1@gmail.com>
2024-08-19 20:42:16 +0400

    [feature] update sql functions

--- web/migrations/Version20240817113102.php
+++ web/migrations/Version20240817113102.php
@@ -19,32 +19,58 @@ final class Version20240817113102 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('
-        DELIMITER //
-        CREATE PROCEDURE count_tickets_service(IN service_id INT)
-        BEGIN
-            SELECT 
-                (SELECT COUNT(*) FROM ticket WHERE service_id = service_id) AS total,
-                (SELECT COUNT(*) FROM ticket WHERE service_id = service_id AND result_date IS NULL) AS in_progress,
-                (SELECT COUNT(*) FROM ticket WHERE service_id = service_id AND result_date IS NOT NULL) AS closed;
-        END //
-        DELIMITER ;
-    ');
+        $this->addSql("DELETE FROM doctrine_migration_versions WHERE version LIKE '%Version20240817113102';");
 
         $this->addSql('
-        DELIMITER //
-        CREATE PROCEDURE count_tickets_user(IN user_id INT)
+        CREATE TRIGGER IF NOT EXISTS `treatment_after_update` AFTER UPDATE ON `treatment` FOR EACH ROW 
+        BEGIN
+             UPDATE ticket
+                SET 
+                    ticket.result_date = (SELECT MAX(treatment.end_date)
+                                              FROM treatment
+                                              WHERE treatment.ticket_id = NEW.ticket_id
+                                                AND treatment.end_date IS NOT NULL),
+                     ticket.result = (SELECT treatment.observations
+                                         FROM treatment
+                                         WHERE treatment.end_date = (SELECT MAX(treatment.end_date)
+                                                                     FROM treatment
+                                                                     WHERE treatment.ticket_id = NEW.ticket_id
+                                                                       AND treatment.end_date IS NOT NULL)
+                                           AND treatment.ticket_id = NEW.ticket_id)
+                WHERE ticket.id = NEW.ticket_id AND NEW.end_date IS NOT NULL;
+        END        
+        ');
+        $this->addSql("
+        CREATE PROCEDURE IF NOT EXISTS  count_tickets_service(IN serviceId INT)
+        BEGIN
+            SELECT
+                COUNT(CASE WHEN t.id NOT IN (SELECT DISTINCT ticket_id FROM treatment)
+                            OR latest_treatment.status = 'EN ATTENTE' THEN 1 END) AS in_waiting,
+                COUNT(CASE WHEN latest_treatment.status = 'EN COURS' THEN 1 END) AS in_progress,
+                COUNT(CASE WHEN latest_treatment.status = 'TERMINÉ' THEN 1 END) AS closed
+            FROM ticket t
+            LEFT JOIN (
+                SELECT ticket_id, status
+                FROM treatment t1
+                WHERE t1.end_date = (
+                    SELECT MAX(t2.end_date)
+                    FROM treatment t2
+                    WHERE t2.ticket_id = t1.ticket_id
+                )
+            ) latest_treatment ON t.id = latest_treatment.ticket_id
+            WHERE t.service_id = serviceId;
+        END
+        ");
+
+        $this->addSql('
+        CREATE PROCEDURE IF NOT EXISTS count_tickets_user(IN userId INT)
         BEGIN
             SELECT 
-                (SELECT COUNT(*) FROM treatment WHERE user_id = user_id) AS n_open,
-                (SELECT COUNT(*) FROM ticket WHERE creator_id = user_id) AS n_create,
-                (SELECT COUNT(*) 
-                 FROM treatment t
-                 JOIN ticket ti ON t.ticket_id = ti.id
-                 WHERE t.user_id = user_id AND t.end_date = ti.result_date) AS n_close;
-        END //
-        DELIMITER ;
-    ');
+            (SELECT COUNT(*) FROM treatment WHERE caterer_id = userId) AS n_open,
+            (SELECT COUNT(*) FROM ticket WHERE creator_id = userId) AS n_create,
+            (SELECT COUNT(*) FROM treatment t JOIN ticket ti ON t.ticket_id = ti.id WHERE t.caterer_id = userId AND t.end_date = ti.result_date AND t.`status` = "TERMINÉ") AS n_close;
+        END
+        ');
     }
 
     public function down(Schema $schema): void
