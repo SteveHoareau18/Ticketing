49dbf82de1e7c1457e55f0c86c2855d0ad9b5cec
Steve <steve.hoareau1@gmail.com>
2024-08-29 17:31:14 +0400

    [feature] add constraints

--- web/src/Form/TicketType.php
+++ web/src/Form/TicketType.php
@@ -8,8 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class TicketType extends AbstractType
 {
@@ -19,18 +19,21 @@ class TicketType extends AbstractType
             ->add('problem', TextareaType::class, [
                 "label" => "Raison de l'ouverture du ticket",
                 'label_attr' => ['class' => 'label'],
-                'attr' => ['class' => 'input input-bordered', 'placeholder' => 'Le client CL001 a besoin de...'],
+                'attr' => [
+                    'class' => 'input input-bordered',
+                    'placeholder' => 'Le client CL001 a besoin de...'
+                ],
                 "mapped" => true,
                 "required" => true,
                 "trim" => true,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'La raison de l\'ouverture du ticket ne peut pas être vide.',
+                    new Assert\NotBlank([
+                        'message' => 'Ce champ ne peut pas être vide.',
                     ]),
-                    new Length([
+                    new Assert\Length([
                         'min' => 10,
                         'minMessage' => 'La raison doit contenir au moins {{ limit }} caractères.',
-                        'max' => 500,
+                        'max' => 1000,
                         'maxMessage' => 'La raison ne peut pas dépasser {{ limit }} caractères.',
                     ]),
                 ],
--- web/src/Form/UserType.php
+++ web/src/Form/UserType.php
@@ -8,6 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
@@ -20,7 +24,20 @@ class UserType extends AbstractType
                 'attr' => ['class' => 'input input-bordered', 'placeholder' => 'jdoe'],
                 "mapped" => true,
                 "required" => true,
-                "trim" => true
+                "trim" => true,
+                'constraints' => [
+                    new NotBlank(['message' => "Le nom d'utilisateur est obligatoire."]),
+                    new Length([
+                        'min' => 4,
+                        'max' => 25,
+                        'minMessage' => "Le nom d'utilisateur doit contenir au moins {{ limit }} caractères.",
+                        'maxMessage' => "Le nom d'utilisateur ne peut pas dépasser {{ limit }} caractères."
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9_]+$/',
+                        'message' => "Le nom d'utilisateur ne peut contenir que des lettres, des chiffres et des underscores."
+                    ]),
+                ]
             ])
             ->add('email', EmailType::class, [
                 "label" => "E-Mail",
@@ -28,7 +45,11 @@ class UserType extends AbstractType
                 'attr' => ['class' => 'input input-bordered', 'placeholder' => 'jdoe@gmail.com'],
                 "mapped" => true,
                 "required" => true,
-                "trim" => true
+                "trim" => true,
+                'constraints' => [
+                    new NotBlank(['message' => "L'adresse e-mail est obligatoire."]),
+                    new Email(['message' => "Veuillez saisir une adresse e-mail valide."]),
+                ]
             ])
             ->add('name', TextType::class, [
                 "label" => "Nom",
@@ -36,7 +57,16 @@ class UserType extends AbstractType
                 'attr' => ['class' => 'input input-bordered', 'placeholder' => 'Doe'],
                 "mapped" => true,
                 "required" => true,
-                "trim" => true
+                "trim" => true,
+                'constraints' => [
+                    new NotBlank(['message' => "Le nom est obligatoire."]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => "Le nom doit contenir au moins {{ limit }} caractères.",
+                        'maxMessage' => "Le nom ne peut pas dépasser {{ limit }} caractères."
+                    ]),
+                ]
             ])
             ->add('firstname', TextType::class, [
                 "label" => "Prénom",
@@ -44,7 +74,16 @@ class UserType extends AbstractType
                 'attr' => ['class' => 'input input-bordered', 'placeholder' => 'John'],
                 "mapped" => true,
                 "required" => true,
-                "trim" => true
+                "trim" => true,
+                'constraints' => [
+                    new NotBlank(['message' => "Le prénom est obligatoire."]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => "Le prénom doit contenir au moins {{ limit }} caractères.",
+                        'maxMessage' => "Le prénom ne peut pas dépasser {{ limit }} caractères."
+                    ]),
+                ]
             ])
             ->add('service', TextType::class, [
                 "label" => "Service",
@@ -52,7 +91,10 @@ class UserType extends AbstractType
                 'attr' => ['class' => 'input input-bordered', 'placeholder' => 'Rechercher...', 'list' => 'serviceLst'],
                 "mapped" => false,
                 "required" => true,
-                "trim" => true
+                "trim" => true,
+                'constraints' => [
+                    new NotBlank(['message' => "Le service est obligatoire."]),
+                ]
             ]);
     }
 
